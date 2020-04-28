c0086f06423f7c670e4bedf2dad38e7d5752e838
fmurrie <felipe_murrie@hotmail.com>
2020-04-27 22:37:58 -0300

    first commit

---
+++ src/main/java/com/utnphones/UTNPhonesDiazFtMurrie/UtnPhonesDiazFtMurrieApplication.java
@@ -0,0 +1,13 @@
+package com.utnphones.UTNPhonesDiazFtMurrie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnPhonesDiazFtMurrieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnPhonesDiazFtMurrieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utnphones/UTNPhonesDiazFtMurrie/UtnPhonesDiazFtMurrieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utnphones.UTNPhonesDiazFtMurrie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnPhonesDiazFtMurrieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
