b20ed0508b9df56d744d480c7b6d5484e73d7305
fmurrie <felipe_murrie@hotmail.com>
2020-05-03 11:59:31 -0300

    Tablas agregadas pero faltan constraints y defaults

---
+++ src/DatabaseScripts/Tables/202005031124_rates.sql
@@ -0,0 +1,49 @@
+/*
+ * 	Author: Murrie Battistoni Felipe
+ * 	Created Date: 03/05/2020
+ * 	Description: create of table rates
+ */
+
+use utnphones;
+
+drop table if exists rates;
+
+create table if not exists rates
+(
+	creatorUser varchar(100),
+	createdDate datetime default(now()),
+	updaterUser varchar(100),
+	updatedDate timestamp,
+	idOriginCity int not null,
+	idDestinityCity int not null,
+	minutePrice varchar(50) not null,
+	constraint PK_rates_idOriginCity_idDestinityCity primary key(idOriginCity,idDestinityCity),
+	constraint FK_rates_idOriginCity foreign key(idOriginCity) references cities(idCity) on update cascade on delete cascade,
+	constraint FK_rates_idDestinityCity foreign key(idDestinityCity) references cities(idCity) on update cascade on delete cascade
+);
+
+/*Triggers for audit*/
+
+/*Trigger for creator user*/
+
+drop trigger if exists rates_audit_creator;
+delimiter //
+create trigger if not exists rates_audit_creator
+before insert on rates
+for each row
+begin
+set new.creatorUser=getDbUserName();
+end //
+delimiter ;
+
+/*Trigger for updater user*/
+
+drop trigger if exists rates_audit_updater;
+delimiter //
+create trigger if not exists rates_audit_updater
+before update on rates
+for each row
+begin
+set new.updaterUser=getDbUserName();
+end //
+delimiter ;
---
+++ src/DatabaseScripts/Tables/202005031155_calls.sql
@@ -0,0 +1,59 @@
+/*
+ * 	Author: Murrie Battistoni Felipe
+ * 	Created Date: 03/05/2020
+ * 	Description: create of table calls
+ */
+
+use utnphones;
+
+drop table if exists calls;
+
+create table if not exists calls
+(
+	creatorUser varchar(100),
+	createdDate datetime default(now()),
+	updaterUser varchar(100),
+	updatedDate timestamp,
+	idCall int auto_increment,
+	idBill int,
+	idPhoneLineOrigin int,
+	idPhoneLineDestinity int,
+	initTime datetime not null,
+	endTime datetime,
+	duration float,
+	totalPrice float,
+	constraint PK_calls_idCall primary key(idCall),
+	constraint CHK_calls_idPhoneLineOrigin_not_null check(idPhoneLineOrigin!=null),
+	constraint CHK_calls_idPhoneLineDestinity_not_null check(idPhoneLineDestinity!=null),
+	constraint CHK_calls_idPhoneLines_not_equal check(idPhoneLineOrigin!=idPhoneLineDestinity),
+	constraint CHK_calls_initTime_bigger_than_endTime check((initTime>=endTime) or ((initTime!=null) and (endTime=null))),
+	constraint FK_calls_idBill foreign key(idBill) references bills(idBill) on update cascade on delete cascade,
+	constraint FK_calls_idPhoneLineOrigin foreign key(idPhoneLineOrigin) references phoneLines(idPhoneLine) on update cascade,
+	constraint FK_calls_idPhoneLineDestinity foreign key(idPhoneLineDestinity) references phoneLines(idPhoneLine) on update cascade
+);
+
+/*Triggers for audit*/
+
+/*Trigger for creator user*/
+
+drop trigger if exists calls_audit_creator;
+delimiter //
+create trigger if not exists calls_audit_creator
+before insert on calls
+for each row
+begin
+set new.creatorUser=getDbUserName();
+end //
+delimiter ;
+
+/*Trigger for updater user*/
+
+drop trigger if exists calls_audit_updater;
+delimiter //
+create trigger if not exists calls_audit_updater
+before update on calls
+for each row
+begin
+set new.updaterUser=getDbUserName();
+end //
+delimiter ;
