b4c26e5e3ee813bc2988f41801d0c230ea3ac039
fmurrie <felipe_murrie@hotmail.com>
2020-05-10 11:04:52 -0300

    tambien en el .bat

--- src/DatabaseScripts/executerDB.bat
+++ src/DatabaseScripts/executerDB.bat
@@ -30,6 +30,9 @@ xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Tables\202005030139_bills.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Tables\202005031124_rates.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Tables\202005031155_calls.sql
 
+REM Section for execute the scripts from \Functions\CustomFunctions:
+
+
 REM Section for execute the scripts from \Procedures:
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Procedures\202005091943_users_insert.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Procedures\202005091945_users_delete.sql
@@ -42,13 +45,13 @@ REM Section for execute the scripts from \Migration:
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005061800_migration_userTypes.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005061802_migration_billStates.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005061803_migration_lineTypes.sql
+xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005061804_migration_callTypes.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005061821_migration_interAreaCodes.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005062121_migration_countryAreaCodes.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005070005_migration_countries.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005071538_migration_provinces.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005071547_migration_localAreaCodes.sql
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005071548_migration_cities.sql
-xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\Migration\202005100006_migration_callTypes.sql
 
 REM End-status of the implementation of the database utnphones:
 xampp\mysql\bin\mysql -u root<%PROJECT_PATH%\General\999999999999_endstatus.sql
