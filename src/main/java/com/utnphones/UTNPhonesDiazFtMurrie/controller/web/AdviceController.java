67cdc9e112e6ca4d09cb3cfd21d5e786f344882e
Ricardo Diaz <rikideriverplate@gmail.com>
2020-06-14 19:52:40 -0300

    Web package added with their corresponded classes

--- src/main/java/com/utnphones/UTNPhonesDiazFtMurrie/controller/web/AdviceController.java
+++ src/main/java/com/utnphones/UTNPhonesDiazFtMurrie/controller/web/AdviceController.java
@@ -1,17 +1,14 @@
 package com.utnphones.UTNPhonesDiazFtMurrie.controller.web;
 
 import com.utnphones.UTNPhonesDiazFtMurrie.dto.ErrorResponseDto;
-import com.utnphones.UTNPhonesDiazFtMurrie.exception.InvalidLoginException;
-import com.utnphones.UTNPhonesDiazFtMurrie.exception.SessionNotExistsException;
-import com.utnphones.UTNPhonesDiazFtMurrie.exception.UserNotexistException;
-import com.utnphones.UTNPhonesDiazFtMurrie.exception.ValidationException;
-import org.hibernate.SessionException;
+import com.utnphones.UTNPhonesDiazFtMurrie.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 
 
 @RestControllerAdvice
@@ -20,23 +17,37 @@ public class AdviceController extends ResponseEntityExceptionHandler {
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(InvalidLoginException.class)
     public ErrorResponseDto handleLoginException(InvalidLoginException exc) {
-        return new ErrorResponseDto(1, "Invalid login");
+        return new ErrorResponseDto(401, "Invalid login");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ValidationException.class)
     public ErrorResponseDto handleValidationException(ValidationException exc) {
-        return new ErrorResponseDto(2, exc.getMessage());
+        return new ErrorResponseDto(400, exc.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserNotexistException.class)
-    public ErrorResponseDto handleUserNotExists() {
-        return new ErrorResponseDto(3, "User not exists");
+    public ErrorResponseDto handleUserNotExists(UserNotexistException exc) {
+        return new ErrorResponseDto(400, exc.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(SessionNotExistsException.class)
     public ErrorResponseDto handleSessionNotExists(SessionNotExistsException sessionNotExistsException) {
         return new ErrorResponseDto(400, sessionNotExistsException.getMessage());
     }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AddUserException.class)
+    public ErrorResponseDto handleAddUserException(AddUserException exc) {
+        return new ErrorResponseDto(401, exc.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserTypeNotExistsException.class)
+    public ErrorResponseDto handleUserTypeNotExists() {
+        return new ErrorResponseDto(400, "ERROR! UserType does not exists");
+    }
+
 }
