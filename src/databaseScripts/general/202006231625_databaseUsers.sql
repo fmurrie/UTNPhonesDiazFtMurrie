42be3a4844e00b33219b77d785d17b41c8d2f3cc
fmurrie <felipe_murrie@hotmail.com>
2020-06-25 18:15:21 -0300

    some fixes

--- src/databaseScripts/general/202006231625_databaseUsers.sql
+++ src/databaseScripts/general/202006231625_databaseUsers.sql
@@ -17,18 +17,6 @@ grant select on utnphones.bills to 'clientUser'@'localhost' IDENTIFIED BY 'clien
 
 FLUSH privileges;
 
-revoke select, update on utnphones.users from 'clientUser'@'localhost' IDENTIFIED BY 'clientPass';
-revoke select on utnphones.phonelines from 'clientUser'@'localhost' IDENTIFIED BY 'clientPass';
-revoke select on utnphones.calls from 'clientUser'@'localhost' IDENTIFIED BY 'clientPass';
-revoke select on utnphones.bills from 'clientUser'@'localhost' IDENTIFIED BY 'clientPass';
-
-SHOW grants for 'clientUser'@'localhost';
-
-select * from mysql.`user` u ;
-
-drop user 'clientUser'@'localhost';
-
-
 ########################################BACKOFFICE_USER############################################
 CREATE user if not exists 'backofficeUser'@'localhost' IDENTIFIED BY 'backofficePass';
 
@@ -40,33 +28,12 @@ grant select on utnphones.bills to 'backofficeUser'@'localhost' IDENTIFIED BY 'b
 
 FLUSH privileges;
 
-revoke insert, select, update, delete on utnphones.users from 'backofficeUser'@'localhost' IDENTIFIED BY 'backofficeUser';
-revoke insert, select, update, delete on utnphones.phonelines from 'backofficeUser'@'localhost' IDENTIFIED BY 'backofficeUser';
-revoke select on utnphones.rates from 'backofficeUser'@'localhost' IDENTIFIED BY 'backofficeUser';
-revoke select on utnphones.calls from 'backofficeUser'@'localhost' IDENTIFIED BY 'backofficeUser';
-revoke select on utnphones.bills from 'backofficeUser'@'localhost' IDENTIFIED BY 'backofficeUser';
-
-SHOW grants for 'backofficeUser'@'localhost';
-
-select * from mysql.`user` u ;
-
-drop user 'backofficeUser'@'localhost';
-
-
 ########################################INFRASTRUCTURE_USER############################################
 CREATE USER'infrastructureUser'@'localhost' IDENTIFIED BY 'infrastructurePass';
 
 grant insert on utnphones.calls to 'infrastructureUser'@'localhost' IDENTIFIED BY 'infrastructurePass';
 
 FLUSH privileges;
-
-revoke insert on utnphones.calls from 'infrastructureUser'@'localhost';
-
-SHOW grants for 'infrastructureUser'@'localhost';
-
-select * from mysql.`user` u ;
-
-drop user 'infrastructureUser'@'localhost';
 
 
 
